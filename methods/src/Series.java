bff5810524547ae206456287adc0d460f245d6ea
rengreen <r.m.gardzinska@gmail.com>
2018-12-27 23:15:13 +0100

    Sum of squares of first n natural numbers

--- methods/src/Series.java
+++ methods/src/Series.java
@@ -4,11 +4,20 @@ public class Series {
         int n=10;
         System.out.println("Sum of first "+n+" natural numbers");
         System.out.println(sumOfNaturalNumbers(n));
+
+        System.out.println("Sum of squares of first "+n+" natural numbers");
+        System.out.println(sumOfNaturalSquares(n));
     }
 
     //Sum of first n natural numbers
     public static int sumOfNaturalNumbers(int n){
         int sum=n*(n+1)/2;
+        return sum;
+    }
+
+    //Sum of squares of first n natural numbers
+    public static int sumOfNaturalSquares(int n){
+        int sum=n*(n+1)*(2*n+1)/6;
         return sum;
     }
 }
