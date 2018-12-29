8fd9dee931db098d2f2be11a62fb0988c22dae63
rengreen <r.m.gardzinska@gmail.com>
2018-12-29 22:28:25 +0100

    n first Fibonacci numbers

---
+++ methods/src/FibonacciSequence.java
@@ -0,0 +1,18 @@
+public class FibonacciSequence {
+
+    public static void main(String[] args) {
+        //0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181
+
+        int n = 10;
+        //n first Fibonacci numbers
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i + ": " + fibonacciElement(i));
+        }
+    }
+
+    public static int fibonacciElement(int n){
+        if (n <= 1)
+            return n;
+        return fibonacciElement(n - 1) + fibonacciElement(n - 2);
+    }
+}
