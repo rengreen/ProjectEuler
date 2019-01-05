f12b8ec8cf50ec4c6190b0522dacb9c40b630f85
rengreen <r.m.gardzinska@gmail.com>
2019-01-05 20:10:50 +0100

    factorial for integers<=12, using recursion

--- methods/src/Factorial.java
+++ methods/src/Factorial.java
@@ -1,10 +1,12 @@
 public class Factorial {
 
     public static void main(String[] args) {
-        int n = 12;
-
+        int n=12;
         System.out.println("factorial of "+n+" using iteration (method for numbers <= 12)");
         System.out.println(factorial(n));
+
+        System.out.println("factorial of "+n+" using recursion (method for numbers <= 12)");
+        System.out.println(factorialRecursive(n));
     }
 
     public static int factorial(int n) {
@@ -14,5 +16,12 @@ public class Factorial {
             product = product * i;
         }
         return product;
+    }
+
+    public static long factorialRecursive(int n) {
+        if (n <= 1)
+            return 1;
+        else
+            return n * factorialRecursive(n - 1);
     }
 }
